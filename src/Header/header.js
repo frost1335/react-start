356be74ae863d61ce4982957a80e3184660658e7
raximvodil <65526165+raximvodil@users.noreply.github.com>
2021-09-05 11:30:18 +0500

    added !

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,12 +1,15 @@
 import React from "react";
-import "./header";
+import "./header.css";
 
 const header = () => {
-    return(
-        <div>
-            header section !
-        </div>
-    )
-}
+  return (
+    <header>
+      <div className={"headerContent"}>
+        <h1 className={"headerTitle"}>Aksiya banneri</h1>
+        <span className={"headerCheck"}>30%</span>
+      </div>
+    </header>
+  );
+};
 
-export default header
+export default header;
